3f78a5a3745d785acb33155d6f6ef33f050b17ab
Darshani_Kaushalya_Diwakara <darshani@gapstars.net>
2022-06-21 20:28:35 +0530

    feat: add Mongo Uri

--- mern-back-end/server/database/database.js
+++ mern-back-end/server/database/database.js
@@ -3,23 +3,23 @@ const mongoose = require("mongoose");
  * MongoDB database connection
  */
 const connectDatabase = async () => {
-    try {
-        //MongoDB Cloud
-        // await mongoose.connect(process.env.MONGO_URI, {
-        //     useNewUrlParser: true,
-        //     useUnifiedTopology: true
-        // });
+  try {
+    //MongoDB Cloud
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-        //Local DB
-        await mongoose.connect(process.env.LOCAL_URI, { useNewUrlParser: true });
+    //Local DB
+    // await mongoose.connect(process.env.LOCAL_URI, { useNewUrlParser: true });
 
-        console.log("mongoDB is connected")
-    } catch (err) {
-        console.log({
-            message: 'Unable to connect to the database',
-            err,
-        });
-    }
+    console.log("mongoDB is connected");
+  } catch (err) {
+    console.log({
+      message: "Unable to connect to the database",
+      err,
+    });
+  }
 };
 
 module.exports = connectDatabase;
